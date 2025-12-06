4df1f0b40cf4356e91aa4cdb3cc4cd69b498adde
chimkentec <chimkentec@gmail.com>
2025-12-06 18:26:13 +0500

    Hide action buttons until order has lines

--- OrdersCreator.UI/MainForm.Designer.cs
+++ OrdersCreator.UI/MainForm.Designer.cs
@@ -258,10 +258,11 @@
             btnCreateReport.Size = new Size(209, 35);
             btnCreateReport.TabIndex = 1;
             btnCreateReport.Text = "СОЗДАТЬ ОТЧЕТ (F12)";
+            btnCreateReport.Visible = false;
             btnCreateReport.UseVisualStyleBackColor = false;
-            // 
+            //
             // btnCancel
-            // 
+            //
             btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             btnCancel.BackColor = Color.Red;
             btnCancel.FlatAppearance.BorderSize = 0;
@@ -273,6 +274,7 @@
             btnCancel.Size = new Size(142, 35);
             btnCancel.TabIndex = 0;
             btnCancel.Text = "ОТМЕНА (DEL)";
+            btnCancel.Visible = false;
             btnCancel.UseVisualStyleBackColor = false;
             // 
             // panelMainBody
