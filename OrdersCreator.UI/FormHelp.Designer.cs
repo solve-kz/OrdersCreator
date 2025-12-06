ebcedc21b39a18410a6968888b4e08e9a4470105
solve_kz <chimkentec@gmail.com>
2025-12-06 19:35:38 +0500

    Добавлена форма для справки и сообщение "О программе"

--- OrdersCreator.UI/FormHelp.Designer.cs
+++ OrdersCreator.UI/FormHelp.Designer.cs
@@ -28,18 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Dock = DockStyle.Fill;
+            webView21.Location = new Point(0, 0);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(884, 561);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
             // 
             // FormHelp
             // 
+            AllowDrop = true;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(884, 561);
+            Controls.Add(webView21);
+            MinimizeBox = false;
             Name = "FormHelp";
-            Text = "FormHelp";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Справка";
+            Load += HelpForm_Load;
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
