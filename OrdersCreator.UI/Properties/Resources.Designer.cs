120e8076f9e4622aa8068ee92123d830432c3cac
solve_kz <chimkentec@gmail.com>
2025-12-08 01:11:14 +0500

    Установлен пакет автообновлений

--- OrdersCreator.UI/Properties/Resources.Designer.cs
+++ OrdersCreator.UI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace OrdersCreator.UI.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap barcode_scanner {
+            get {
+                object obj = ResourceManager.GetObject("barcode-scanner", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap categories {
             get {
                 object obj = ResourceManager.GetObject("categories", resourceCulture);
