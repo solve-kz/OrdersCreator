63cbc6bee626f0d1efc296f3d6e01bb4fe6bb7f3
solve_kz <chimkentec@gmail.com>
2025-12-07 19:03:56 +0500

    Добавлена иконка настроек

--- OrdersCreator.UI/Properties/Resources.Designer.cs
+++ OrdersCreator.UI/Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace OrdersCreator.UI.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icon_settings {
+            get {
+                object obj = ResourceManager.GetObject("icon_settings", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap new_file {
             get {
                 object obj = ResourceManager.GetObject("new-file", resourceCulture);
